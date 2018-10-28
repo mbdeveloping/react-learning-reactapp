039718638d66b19a5652c5e385af061fe87eae3b
Mantvydas <mantvydas.binderis@gmail.com>
2018-10-28 09:08:39 +0000

    setting up react and making first element

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = React.createElement(
+    'h1',
+    {id: 'main-title', title: 'This is a title.'},
+    'My first React element!'
+  );
+
+  const desc = React.createElement(
+      'p',
+      null,
+      'I jus learned how to create a react node and render into the DOM.'
+  );
+
+  const header = React.createElement(
+      'header',
+      null,
+      title,
+      desc
+  );
+
+ReactDOM.render(
+    header, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
